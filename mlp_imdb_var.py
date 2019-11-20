b04a5f7e3f06a59e38c5d6cd7fa0bc46e43dddd7
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-11-20 22:20:10 +0530

    Regularization Added

--- mlp_imdb_var.py
+++ mlp_imdb_var.py
@@ -9,7 +9,7 @@ import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-from keras import models, layers
+from keras import models, layers, regularizers
 from keras.datasets import imdb
 
 # save np.load
@@ -116,3 +116,48 @@ plt.legend()
 
 plt.show()
 
+"""
+Adding weight regularization
+
+L1 regularization, where the cost added is proportional to the absolute value of the weights coefficients (i.e. to what is called the "L1 norm" of the weights).
+
+L2 regularization, where the cost added is proportional to the square of the value of the weights coefficients (i.e. to what is called the "L2 norm" of the weights). 
+L2 regularization is also called weight decay in the context of neural networks. 
+Don't let the different name confuse you: weight decay is mathematically the exact same as L2 regularization.
+"""
+
+l2_model = models.Sequential()
+l2_model.add(layers.Dense(16, kernel_regularizer=regularizers.l2(0.001),
+                          activation='relu', input_shape=(10000,)))
+l2_model.add(layers.Dense(16, kernel_regularizer=regularizers.l2(0.001),
+                          activation='relu'))
+l2_model.add(layers.Dense(1, activation='sigmoid'))
+
+l2_model.compile(optimizer='rmsprop',
+                 loss='binary_crossentropy',
+                 metrics=['acc'])
+
+l2_model_hist = l2_model.fit(x_train, y_train,
+                             epochs=20,
+                             batch_size=512,
+                             validation_data=(x_test, y_test))
+
+l2_model_val_loss = l2_model_hist.history['val_loss']
+
+plt.plot(epochs, original_val_loss, 'b+', label='Original model')
+plt.plot(epochs, l2_model_val_loss, 'bo', label='L2-regularized model')
+plt.xlabel('Epochs')
+plt.ylabel('Validation loss')
+plt.legend()
+
+plt.show()
+
+"""
+As alternatives to L2 regularization, we could use one of the following Keras weight regularizers:
+
+# L1 regularization
+regularizers.l1(0.001)
+
+# L1 and L2 regularization at the same time
+regularizers.l1_l2(l1=0.001, l2=0.001)
+"""
