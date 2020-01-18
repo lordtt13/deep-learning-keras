e89b89752b127bc0cf8ef0dc16d2fef44401f4be
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-01-19 01:00:32 +0530

    Advanced LSTM Baselines Established

--- lstm_adv.py
+++ lstm_adv.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras import layers
 from keras.optimizers import RMSprop
+from keras.datasets import imdb
+from keras import backend as K
 
 
 data_dir = './'
@@ -292,5 +294,70 @@ history = model.fit_generator(train_gen_reverse,
                               validation_data=val_gen_reverse,
                               validation_steps=val_steps)
 
+max_features = 10000
+maxlen = 500
 
+# save np.load
+np_load_old = np.load
 
+# modify the default parameters of np.load
+np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+
+# call load_data with allow_pickle implicitly set to true
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+
+# restore np.load for future normal usage
+np.load = np_load_old
+
+# Reverse sequences
+x_train = [x[::-1] for x in x_train]
+x_test = [x[::-1] for x in x_test]
+
+# Pad sequences
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+
+model = Sequential()
+model.add(layers.Embedding(max_features, 128))
+model.add(layers.LSTM(32))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+history = model.fit(x_train, y_train,
+                    epochs=10,
+                    batch_size=128,
+                    validation_split=0.2)
+
+K.clear_session()
+
+model = Sequential()
+model.add(layers.Embedding(max_features, 32))
+model.add(layers.Bidirectional(layers.LSTM(32)))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(x_train, y_train, epochs=10, batch_size=128, validation_split=0.2)
+
+model = Sequential()
+model.add(layers.Bidirectional(
+    layers.GRU(32), input_shape=(None, float_data.shape[-1])))
+model.add(layers.Dense(1))
+
+model.compile(optimizer=RMSprop(), loss='mae')
+history = model.fit_generator(train_gen,
+                              steps_per_epoch=500,
+                              epochs=40,
+                              validation_data=val_gen,
+                              validation_steps=val_steps)model = Sequential()
+model.add(layers.Bidirectional(
+    layers.GRU(32), input_shape=(None, float_data.shape[-1])))
+model.add(layers.Dense(1))
+
+model.compile(optimizer=RMSprop(), loss='mae')
+history = model.fit_generator(train_gen,
+                              steps_per_epoch=500,
+                              epochs=40,
+                              validation_data=val_gen,
+                              validation_steps=val_steps)
