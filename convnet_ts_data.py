03270a52c4f7ac4e22046737d35a2bcee9f9441f
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-01-19 01:10:37 +0530

    Added convnets+gru for time series data

--- convnet_ts_data.py
+++ convnet_ts_data.py
@@ -185,3 +185,63 @@ plt.title('Training and validation loss')
 plt.legend()
 
 plt.show()
+
+# This was previously set to 6 (one point per hour).
+# Now 3 (one point per 30 min).
+step = 3
+lookback = 720  # Unchanged
+delay = 144 # Unchanged
+
+train_gen = generator(float_data,
+                      lookback=lookback,
+                      delay=delay,
+                      min_index=0,
+                      max_index=200000,
+                      shuffle=True,
+                      step=step)
+val_gen = generator(float_data,
+                    lookback=lookback,
+                    delay=delay,
+                    min_index=200001,
+                    max_index=300000,
+                    step=step)
+test_gen = generator(float_data,
+                     lookback=lookback,
+                     delay=delay,
+                     min_index=300001,
+                     max_index=None,
+                     step=step)
+val_steps = (300000 - 200001 - lookback) // 128
+test_steps = (len(float_data) - 300001 - lookback) // 128
+
+# Conv1D + GRU
+model = Sequential()
+model.add(layers.Conv1D(32, 5, activation='relu',
+                        input_shape=(None, float_data.shape[-1])))
+model.add(layers.MaxPooling1D(3))
+model.add(layers.Conv1D(32, 5, activation='relu'))
+model.add(layers.GRU(32, dropout=0.1, recurrent_dropout=0.5))
+model.add(layers.Dense(1))
+
+model.summary()
+
+model.compile(optimizer=RMSprop(), loss='mae')
+history = model.fit_generator(train_gen,
+                              steps_per_epoch=500,
+                              epochs=20,
+                              validation_data=val_gen,
+                              validation_steps=val_steps)
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(loss))
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
