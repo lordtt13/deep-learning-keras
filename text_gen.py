18d5d74865c96976198f5471ef2e7b5dba4d5090
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-01-19 19:28:37 +0530

    Text Generation Initial Commit

---
+++ text_gen.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 11:01:46 2020
+
+@author: Tanmay Thakur
+"""
+
+import keras
+import numpy as np
+import sys
+import random
+
+from keras import layers
+
+
+path = keras.utils.get_file(
+    'nietzsche.txt',
+    origin='https://s3.amazonaws.com/text-datasets/nietzsche.txt')
+text = open(path).read().lower()
+
+"""
+We will extract partially-overlapping sequences of length maxlen, one-hot encode them and pack them in a 3D Numpy array x of shape (sequences, maxlen, unique_characters). 
+Simultaneously, we prepare a array y containing the corresponding targets: the one-hot encoded characters that come right after each extracted sequence.
+"""
+# Length of extracted character sequences
+maxlen = 60
+
+# We sample a new sequence every `step` characters
+step = 3
+
+# This holds our extracted sequences
+sentences = []
+
+# This holds the targets (the follow-up characters)
+next_chars = []
+
+for i in range(0, len(text) - maxlen, step):
+    sentences.append(text[i: i + maxlen])
+    next_chars.append(text[i + maxlen])
+print('Number of sequences:', len(sentences))
+
+# List of unique characters in the corpus
+chars = sorted(list(set(text)))
+print('Unique characters:', len(chars))
+# Dictionary mapping unique characters to their index in `chars`
+char_indices = dict((char, chars.index(char)) for char in chars)
+
+# Next, one-hot encode the characters into binary arrays.
+print('Vectorization...')
+x = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        x[i, t, char_indices[char]] = 1
+    y[i, char_indices[next_chars[i]]] = 1
+    
+model = keras.models.Sequential()
+model.add(layers.LSTM(128, input_shape=(maxlen, len(chars))))
+model.add(layers.Dense(len(chars), activation='softmax'))
+
+optimizer = keras.optimizers.RMSprop(lr=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+"""
+Given a trained model and a seed text snippet, we generate new text by repeatedly:
+
+1) Drawing from the model a probability distribution over the next character given the text available so far
+2) Reweighting the distribution to a certain "temperature"
+3) Sampling the next character at random according to the reweighted distribution
+4) Adding the new character at the end of the available text
+    
+This is the code we use to reweight the original probability distribution coming out of the model, and draw a character index from it (the "sampling function"):
+"""
+
+def sample(preds, temperature=1.0):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
