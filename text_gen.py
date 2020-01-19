195ae4607973e1f79952616e1661f763c81942ba
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-01-19 19:31:29 +0530

    Text Generation Prediction Add

--- text_gen.py
+++ text_gen.py
@@ -80,3 +80,35 @@ def sample(preds, temperature=1.0):
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
 
+for epoch in range(1, 60):
+    print('epoch', epoch)
+    # Fit the model for 1 epoch on the available training data
+    model.fit(x, y,
+              batch_size=128,
+              epochs=1)
+
+    # Select a text seed at random
+    start_index = random.randint(0, len(text) - maxlen - 1)
+    generated_text = text[start_index: start_index + maxlen]
+    print('--- Generating with seed: "' + generated_text + '"')
+
+    for temperature in [0.2, 0.5, 1.0, 1.2]:
+        print('------ temperature:', temperature)
+        sys.stdout.write(generated_text)
+
+        # We generate 400 characters
+        for i in range(400):
+            sampled = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(generated_text):
+                sampled[0, t, char_indices[char]] = 1.
+
+            preds = model.predict(sampled, verbose=0)[0]
+            next_index = sample(preds, temperature)
+            next_char = chars[next_index]
+
+            generated_text += next_char
+            generated_text = generated_text[1:]
+
+            sys.stdout.write(next_char)
+            sys.stdout.flush()
+        print()
