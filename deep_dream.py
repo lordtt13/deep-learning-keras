e79d1d2eb91c182a4f13c823d42d207494b9fd9d
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-01-19 20:39:22 +0530

    Function Executed for Deep Dream

--- deep_dream.py
+++ deep_dream.py
@@ -6,9 +6,13 @@ Created on Sun Jan 19 19:33:49 2020
 """
 
 # Implementing Deep Dream in Keras
+import scipy
+import numpy as np
+import matplotlib.pyplot as plt
 
 from keras.applications import inception_v3
 from keras import backend as K
+from keras.preprocessing import image
 
 
 # We will not be training our model,
@@ -76,3 +80,92 @@ def gradient_ascent(x, iterations, step, max_loss=None):
         print('...Loss value at', i, ':', loss_value)
         x += step * grad_values
     return x
+
+def resize_img(img, size):
+    img = np.copy(img)
+    factors = (1,
+               float(size[0]) / img.shape[1],
+               float(size[1]) / img.shape[2],
+               1)
+    return scipy.ndimage.zoom(img, factors, order=1)
+
+
+def save_img(img, fname):
+    pil_img = deprocess_image(np.copy(img))
+    scipy.misc.imsave(fname, pil_img)
+
+
+def preprocess_image(image_path):
+    # Util function to open, resize and format pictures
+    # into appropriate tensors.
+    img = image.load_img(image_path)
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = inception_v3.preprocess_input(img)
+    return img
+
+
+def deprocess_image(x):
+    # Util function to convert a tensor into a valid image.
+    if K.image_data_format() == 'channels_first':
+        x = x.reshape((3, x.shape[2], x.shape[3]))
+        x = x.transpose((1, 2, 0))
+    else:
+        x = x.reshape((x.shape[1], x.shape[2], 3))
+    x /= 2.
+    x += 0.5
+    x *= 255.
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+# Playing with these hyperparameters will also allow you to achieve new effects
+
+step = 0.01  # Gradient ascent step size
+num_octave = 3  # Number of scales at which to run gradient ascent
+octave_scale = 1.4  # Size ratio between scales
+iterations = 20  # Number of ascent steps per scale
+
+# If our loss gets larger than 10,
+# we will interrupt the gradient ascent process, to avoid ugly artifacts
+max_loss = 10.
+
+# Fill this to the path to the image you want to use
+base_image_path = '/home/ubuntu/data/original_photo_deep_dream.jpg'
+
+# Load the image into a Numpy array
+img = preprocess_image(base_image_path)
+
+# We prepare a list of shape tuples
+# defining the different scales at which we will run gradient ascent
+original_shape = img.shape[1:3]
+successive_shapes = [original_shape]
+for i in range(1, num_octave):
+    shape = tuple([int(dim / (octave_scale ** i)) for dim in original_shape])
+    successive_shapes.append(shape)
+
+# Reverse list of shapes, so that they are in increasing order
+successive_shapes = successive_shapes[::-1]
+
+# Resize the Numpy array of the image to our smallest scale
+original_img = np.copy(img)
+shrunk_original_img = resize_img(img, successive_shapes[0])
+
+for shape in successive_shapes:
+    print('Processing image shape', shape)
+    img = resize_img(img, shape)
+    img = gradient_ascent(img,
+                          iterations=iterations,
+                          step=step,
+                          max_loss=max_loss)
+    upscaled_shrunk_original_img = resize_img(shrunk_original_img, shape)
+    same_size_original = resize_img(original_img, shape)
+    lost_detail = same_size_original - upscaled_shrunk_original_img
+
+    img += lost_detail
+    shrunk_original_img = resize_img(original_img, shape)
+    save_img(img, fname='dream_at_scale_' + str(shape) + '.png')
+
+save_img(img, fname='final_dream.png')
+
+plt.imshow(deprocess_image(np.copy(img)))
+plt.show()
