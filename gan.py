86292a018627b4ce0e718c10c7c97c08a132c9b6
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-01-22 00:36:00 +0530

    Generator Add

---
+++ gan.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 00:29:41 2020
+
+@author: Tanmay Thakur
+"""
+
+import keras
+from keras import layers
+import numpy as np
+
+
+latent_dim = 32
+height = 32
+width = 32
+channels = 3
+
+# Generator
+generator_input = keras.Input(shape=(latent_dim,))
+
+# First, transform the input into a 16x16 128-channels feature map
+x = layers.Dense(128 * 16 * 16)(generator_input)
+x = layers.LeakyReLU()(x)
+x = layers.Reshape((16, 16, 128))(x)
+
+# Then, add a convolution layer
+x = layers.Conv2D(256, 5, padding='same')(x)
+x = layers.LeakyReLU()(x)
+
+# Upsample to 32x32
+x = layers.Conv2DTranspose(256, 4, strides=2, padding='same')(x)
+x = layers.LeakyReLU()(x)
+
+# Few more conv layers
+x = layers.Conv2D(256, 5, padding='same')(x)
+x = layers.LeakyReLU()(x)
+x = layers.Conv2D(256, 5, padding='same')(x)
+x = layers.LeakyReLU()(x)
+
+# Produce a 32x32 1-channel feature map
+x = layers.Conv2D(channels, 7, activation='tanh', padding='same')(x)
+generator = keras.models.Model(generator_input, x)
+generator.summary()
